b434beb61f45146fca4ee4bcea1091c2dcff5628
Wechuli <wechulipaul@yahoo.com>
2019-09-02 21:30:27 +0300

    completing the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import Shop from "./pages/shop/ShopPage";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop}/>
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/crown.svg";
+import './NavBar.scss';
+
+function NavBar() {
+  return (
+    <div className="header">
+      <Link className="logo-container" exact to="/">
+        <img src={logo} alt="logo" className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" exact to="/shop">
+          SHOP
+        </Link>
+
+        <Link className="option" exact to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
