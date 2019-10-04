55408cdd46a21591a2600df38d3b16e4f997ca51
Wechuli <wechulipaul@yahoo.com>
2019-10-04 21:07:21 +0300

    fixing nested routes issue

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import CollectionOverview from "../../components/collection-overview/CollectionOverview";
 import CategoryPage from "../category/CategoryPage";
@@ -9,10 +9,10 @@ const Shop = props => {
   console.log(match.path);
   return (
     <div className="shop-page">
-      <Route exact path='/shop' component={CollectionOverview} />
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route exact path="/shop" component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
 
-export default withRouter(Shop);
+export default Shop;
